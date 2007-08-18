0a73482fd958ed19c51441c98d1c221fd2ca2dc1
pat@freelancing-gods.com <pat@freelancing-gods.com@44c325e4-a42c-0410-87d1-89eb90bb738d>
2007-08-18 03:53:13 +0000

    Added comments to conditional caching plugin

--- conditional_caching/lib/action_controller.rb
+++ conditional_caching/lib/action_controller.rb
@@ -1,10 +1,32 @@
+# Changes and additions to ActionController
 module ActionController
   class Base
+    # This method allows blocks to be passed directly to the caches_action
+    # method. I'm not a fan of the syntax (ie. it's not obvious that the block
+    # is for determining whether the action is cached), and I don't like
+    # completely overwriting the default Rails method.
     # def self.caches_action(*actions, &block)
     #   return unless perform_caching
     #   around_filter(ActionCacheFilter.new(*actions, &block))
     # end 
     
+    # A method for checking if a cached fragment exists when a certain
+    # condition is true. The first parameter is that condition, the rest are
+    # the normal parameters for
+    # ActionController::Caching::Fragments.read_fragment
+    #
+    # ==== Examples
+    # A default call, the equivalent of read_fragment()
+    # 
+    #   if conditional_read_fragment(@current_user.nil?)
+    #     # code that should not be run if there's a cache
+    #   end
+    #
+    # Passing additional caching params works in the same way
+    # 
+    #   if conditional_read_fragment(@current_user.nil?, :page => params[:page])
+    #     # ...
+    #   end
     def conditional_read_fragment(conditional, name, options = nil)
       return read_fragment(name, options) if conditional
     end
@@ -13,6 +35,9 @@ module ActionController
   module Caching
     module Actions
       class ActionCacheFilter
+        # This code allows a block from the caches_action method.
+        # See comments above explaining why it's commented out.
+        #
         # alias_method :default_initialize, :initialize
         # 
         # def initialize(*actions, &block)
@@ -22,6 +47,24 @@ module ActionController
         
         alias_method :default_before, :before
         
+        # This method, aliased from the default version, will only
+        # attempt to cache if there's an :if parameter supplied
+        # with the caches_action call. The :if value can be either
+        # a Proc or a symbol pointing to an instance method of the
+        # controller.
+        #
+        # ==== Examples
+        # Using a symbol:
+        #
+        #   caches_action :index, :if => :i_can_has_cache?
+        #   # ...
+        #   def i_can_has_cache?
+        #     Time.now.wday == 1 # only cache on Mondays
+        #   end
+        #
+        # Using a Proc:
+        #
+        #   caches_action :index, :if => Proc.new { Time.now.wday == 1 }
         def before(controller)
           unless check = (@options.delete(:if) || @block)
             return default_before(controller)
--- conditional_caching/lib/action_view.rb
+++ conditional_caching/lib/action_view.rb
@@ -1,7 +1,22 @@
 module ActionView
   module Helpers
     module CacheHelper
-      # Selective fragment caching
+      # This method acts in much the same way as
+      # ActionView::Helpers::CacheHelper.cache - but with the first parameter
+      # acting as a flag to whether it should cache the fragment.
+      #
+      # ==== Examples
+      #
+      #   <% conditional_cache @current_user.nil? do %>
+      #     <!-- view code that will only be cached when @current_user is nil -->
+      #   <% end %>
+      #
+      # Just like the default cache method, it also accepts additional
+      # parameters:
+      #
+      #   <% conditional_cache @current_user.nil?, :page => params[:page] %>
+      #     <!-- view code -->
+      #   <% end %>
       def conditional_cache(conditional, name = {}, &block)
         conditional ? @controller.cache_erb_fragment(block, name) : block.call
       end
