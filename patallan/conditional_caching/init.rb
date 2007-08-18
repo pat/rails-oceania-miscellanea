617058e6bb338a4b6fb9cc87f5b41a94b9d7e2ad
pat@freelancing-gods.com <pat@freelancing-gods.com@44c325e4-a42c-0410-87d1-89eb90bb738d>
2007-08-18 03:10:28 +0000

    Adding Conditional Caching plugin

---
+++ patallan/conditional_caching/init.rb
@@ -0,0 +1,3 @@
+path = File.dirname(File.expand_path(__FILE__))
+require "#{path}/lib/action_controller"
+require "#{path}/lib/action_view"
---
+++ patallan/conditional_caching/tasks/conditional_caching_tasks.rake
@@ -0,0 +1,4 @@
+# desc "Explaining what the task does"
+# task :conditional_caching do
+#   # Task goes here
+# end
