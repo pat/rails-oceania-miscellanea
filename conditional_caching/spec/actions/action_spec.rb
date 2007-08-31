2faf040be52a6e966f825db98a4cf16d99c7a84a
pat@freelancing-gods.com <pat@freelancing-gods.com@44c325e4-a42c-0410-87d1-89eb90bb738d>
2007-08-31 14:34:08 +0000

    Finished action caching spec

--- conditional_caching/spec/actions/action_spec.rb
+++ conditional_caching/spec/actions/action_spec.rb
@@ -1,39 +1,5 @@
 require "spec/spec_helper"
 
-ActionController::Caching::Actions::ActionCacheFilter.send(:include,
-  ConditionalActionCaching)
-
-class ConditionalActionsController < ActionController::Base
-  caches_action :index
-  def index
-    render :text => "index"
-  end
-
-  def show
-    render :text => "show #{params[:id]}"
-  end
-
-  def new
-    render :text => "new"
-  end
-  
-  def create
-    render :text => "create"
-  end
-  
-  def edit
-    render :text => "edit #{params[:id]}"
-  end
-  
-  def update
-    render :text => "update #{params[:id]}"
-  end
-  
-  def destroy
-    render :text => "destroy #{params[:id]}"
-  end
-end
-
 describe "Conditional Action Caching" do
   before :all do
     FileUtils.mkdir_p(FILE_STORE_PATH)
@@ -50,8 +16,92 @@ describe "Conditional Action Caching" do
     @controller.expire_fragment(/hostname.com/)
   end
   
+  after :all do
+    FileUtils.rm_r(FILE_STORE_PATH)
+  end
+  
   it "should cache when there's no condition supplied" do
     get :index
     @request.should have_cache
+  end
+  
+  it "should cache when the condition is set to nil" do
+    get :show, :id => 1
+    @request.should have_cache
+  end
+  
+  it "should cache when the condition is set to a symbol pointer to a method that returns true" do
+    @controller.will_cache = true
+    get :new
+    @request.should have_cache
+  end
+  
+  it "should not cache when the condition is set to a symbol pointer to a method that returns false" do
+    @controller.will_cache = false
+    get :new
+    @request.should_not have_cache
+  end
+  
+  it "should cache when the condition is set to a Proc that returns true" do
+    @controller.will_cache = true
+    get :edit, :id => 1
+    @request.should have_cache
+  end
+  
+  it "should not cache when the condition is set to a Proc that returns false" do
+    @controller.will_cache = false
+    get :edit, :id => 1
+    @request.should_not have_cache
+  end
+  
+  it "should raise ArgumentErrors when the condition is not a symbol or Proc" do
+    post :create
+    @response.body.include?('ArgumentError').should == true
+    put :update, :id => 1
+    @response.body.include?('ArgumentError').should == true
+    delete :destroy, :id => 1
+    @response.body.include?('ArgumentError').should == true
+  end
+end
+
+ActionController::Caching::Actions::ActionCacheFilter.send(:include,
+  ConditionalActionCaching)
+
+class ConditionalActionsController < ActionController::Base
+  attr_accessor :will_cache
+  
+  caches_action :index
+  def index
+    render :text => "index"
+  end
+
+  caches_action :show, :if => nil
+  def show
+    render :text => "show #{params[:id]}"
+  end
+
+  caches_action :new, :if => :will_cache
+  def new
+    render :text => "new"
+  end
+  
+  caches_action :edit, :if => Proc.new { self.will_cache }
+  def edit
+    render :text => "edit #{params[:id]}"
+  end
+  
+  caches_action :create, :if => 1
+  def create
+    render :text => "create"
+  end
+  
+  caches_action :update, :if => "a"
+  def update
+    render :text => "update #{params[:id]}"
+  end
+  
+  caches_action :destroy, :if => Time.now
+  def destroy
+    render :text => "destroy #{params[:id]}"
   end
 end
