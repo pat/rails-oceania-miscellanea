bb969817f850a59566db1d542baeb9caf9892919
pat@freelancing-gods.com <pat@freelancing-gods.com@44c325e4-a42c-0410-87d1-89eb90bb738d>
2007-08-31 02:21:27 +0000

    Neatening requires from init.rb

--- conditional_caching/init.rb
+++ conditional_caching/init.rb
@@ -1,3 +1,2 @@
-path = File.dirname(File.expand_path(__FILE__))
-require "#{path}/lib/action_controller"
-require "#{path}/lib/action_view"
+require "action_controller_additions"
+require "action_view_additions"
