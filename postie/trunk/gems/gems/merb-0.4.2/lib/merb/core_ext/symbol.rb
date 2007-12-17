3907ac9f4ef3408a3c57ee0c3e792f5b21b3848b
pat@freelancing-gods.com <pat@freelancing-gods.com@44c325e4-a42c-0410-87d1-89eb90bb738d>
2007-12-17 03:47:00 +0000

    removing unneccessary gems

--- postie/trunk/gems/gems/merb-0.4.2/lib/merb/core_ext/symbol.rb
+++
@@ -1,6 +0,0 @@
-class Symbol  
-  # ["foo", "bar"].map &:reverse #=> ['oof', 'rab']
-  def to_proc
-     Proc.new{|*args| args.shift.__send__(self, *args)}
-   end
-end
--- postie/trunk/gems/gems/merb-0.4.2/spec/fixtures/foo.rb
+++
@@ -1,3 +0,0 @@
-class Foo
-  attr_accessor :bar
-end  
