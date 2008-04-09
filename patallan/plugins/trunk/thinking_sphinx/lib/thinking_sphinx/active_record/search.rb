ef9713ffdc085b90283a60ef5c4fa58ae665cf4d
pat@freelancing-gods.com <pat@freelancing-gods.com@44c325e4-a42c-0410-87d1-89eb90bb738d>
2008-04-09 13:44:47 +0000

    Docs pretty close to done...

--- patallan/plugins/trunk/thinking_sphinx/lib/thinking_sphinx/active_record/search.rb
+++ patallan/plugins/trunk/thinking_sphinx/lib/thinking_sphinx/active_record/search.rb
@@ -1,12 +1,16 @@
 module ThinkingSphinx
   module ActiveRecord
+    # This module covers the specific model searches - but the syntax is
+    # exactly the same as the core Search class - so use that as your refence
+    # point.
+    # 
     module Search
       def self.included(base)
         base.class_eval do
           class << self
             # Searches for results that match the parameters provided. Will only
-            # return the ids for the matching objects. See #search for syntax
-            # examples.
+            # return the ids for the matching objects. See
+            # ThinkingSphinx::Search#search for syntax examples.
             #
             def search_for_ids(*args)
               options = args.extract_options!
@@ -15,23 +19,8 @@ module ThinkingSphinx
               ThinkingSphinx::Search.search_for_ids(*args)
             end
 
-            # Searches for results that match the parameters provided. These
-            # parameter keys should match the names of fields in the indexes.
-            #
-            # This will use WillPaginate for results if the plugin is installed.
-            # The same parameters - :page and :per_page - work as expected, and
-            # the returned result set can be used by the will_paginate helper.
-            #
-            # Please use only specified attributes when ordering results -
-            # anything else will make the query fall over.
-            #
-            # Examples:
-            #
-            #   Invoice.search :conditions => {:customer => "Pat"}
-            #   Invoice.search "Pat" # search all fields
-            #   Invoice.search "Pat", :page => (params[:page] || 1)
-            #   Invoice.search "Pat", :order => "created_at ASC"
-            #   Invoice.search "Pat", :include => :line_items
+            # Searches for results limited to a single model. See
+            # ThinkingSphinx::Search#search for syntax examples.
             #
             def search(*args)
               options = args.extract_options!
